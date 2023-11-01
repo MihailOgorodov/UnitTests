d6bf5713981ccdbba4ee3d7859d9c5c357278dfe
“Mihail <mic311089@gmail.com>
2023-11-01 17:20:18 +0400

    hw1

---
+++ src/main/java/org/example/HW1/Calculator/CalculatorTest.java
@@ -0,0 +1,123 @@
+package org.example.HW1.Calculator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        // Проверка базового функционала с целыми числами:
+        if (8 != Calculator.calculation(2, 6, '+')) {
+            throw new AssertionError("Ошибка в методе");
+        }
+
+        if (0 != Calculator.calculation(2, 2, '-')) {
+            throw new AssertionError("Ошибка в методе");
+        }
+
+        if (14 != Calculator.calculation(2, 7, '*')) {
+            throw new AssertionError("Ошибка в методе");
+        }
+
+        if (2 != Calculator.calculation(100, 50, '/')) {
+            throw new AssertionError("Ошибка в методе");
+        }
+
+        // Случаи с неправильными аргументами
+        // аргумент operator типа char, должен вызывать исключение, если он получает не базовые символы (+-*/)
+        // try {
+        //     seminars.first.Calculator.Calculator.calculation(8, 4, '_');
+        // } catch (IllegalStateException e) {
+        //     if (!e.getMessage().equals("Unexpected value operator: _")) {
+        //         throw new AssertionError("Ошибка в методе");
+        //     }
+        // }
+
+        // Проверка базового функционала с целыми числами, с использованием утверждений:
+        assert 8 == Calculator.calculation(2, 6, '+');
+        assert 0 == Calculator.calculation(2, 2, '-');
+        assert 14 == Calculator.calculation(2, 7, '*');
+        assert 2 == Calculator.calculation(100, 50, '/');
+
+        // Проверка базового функционала с целыми числами, с использованием утверждений AssertJ:
+//        assertThat(Calculator.calculation(2, 6, '+')).isEqualTo(8);
+//        assertThat(Calculator.calculation(2, 2, '-')).isEqualTo(0);
+//        assertThat(Calculator.calculation(2, 7, '*')).isEqualTo(14);
+//        assertThat(Calculator.calculation(100, 50, '/')).isEqualTo(2);
+
+        // Проверка ожидаемого исключения, с использованием утверждений AssertJ:
+//        assertThatThrownBy(() ->
+//                Calculator.calculation(8, 4, '_')
+//        ).isInstanceOf(IllegalStateException.class);
+
+        System.out.println(Calculator.calculation(2_147_483_647, 1, '+')); // integer overflow
+        System.out.println(Calculator.squareRootExtraction(169));
+
+        // Примерные решения домашних заданий из 1 лекции:
+
+        // HW1.1: Придумайте и опишите (можно в псевдокоде) функцию извлечения корня и
+        // необходимые проверки для него используя граничные случаи
+        // assertThatThrownBy(() ->
+        //         seminars.first.Calculator.Calculator.squareRootExtraction(0, 1, -1)
+        // ).isInstanceOf(SomeStateException.class);
+
+        // HW1.2: Как будет выглядеть проверка для случая деления на ноль? (с использованием AssertJ)
+        // assertThatThrownBy(() ->
+        //        seminars.first.Calculator.Calculator.calculation(5, 0, '/')
+        // ).isInstanceOf(ArithmeticException.class);
+
+        // HW1.3: Сравните одну и ту же проверку с использованием условий, ассертов, AssertJ
+        // в каком случае стандартное сообщение об ошибке будет более информативным?
+        // if (0 != seminars.first.Calculator.Calculator.calculation(2, 6, '+')) {
+        //     throw new AssertionError("Ошибка в методе");
+        // }
+        //   assert 0 == seminars.first.Calculator.Calculator.calculation(2, 6, '+');
+        //    assertThat(seminars.first.Calculator.Calculator.calculation(2, 6, '+')).isEqualTo(0);
+
+
+    }
+    /**
+     * Исключение деления на ноль
+     */
+    @Test
+    public void checkDivisionByZero() {
+        String message = "Division by zero is not possible";
+        Exception exception = assertThrows(ArithmeticException.class, () ->
+                Calculator.calculation(5, 0, '/'));
+        assertEquals(message, exception.getMessage());
+    }
+
+    /**
+     * Исключение извлечения корня из отрицательного числа
+     */
+    @Test
+    public void checkTakingRootNegativeNumber() {
+        String message = "Cannot calculate square root of a negative number";
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->
+                Calculator.squareRootExtraction(-0.1));
+        assertEquals(message, exception.getMessage());
+    }
+
+    /**
+     * Исключение сумма покупок меньше рубля
+     */
+    @Test
+    public void purchaseAmountCannotLessOne() {
+        String messageException = "Сумма покупок не может быть меньше 1 рубля";
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->
+                Calculator.calculatingDiscount(0, 10));
+        assertEquals(messageException, exception.getMessage());
+    }
+
+    /**
+     * Исключение процент скидки меньше нуля
+     */
+    @Test
+    public void discountAmountCannotLessZero() {
+        String message = "Процент скидки не может быть отрицательным";
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->
+                Calculator.calculatingDiscount(1000, -5));
+        assertEquals(message, exception.getMessage());
+    }
+}
